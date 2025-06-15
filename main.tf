2c3e25d5c49d1a1cd90abd01d59245e1da07aa25
mahad ahmed <mahad.ahmed@otmail.com>
2025-06-15 12:04:48 +0100

    created new resources

--- main.tf
+++ main.tf
@@ -1,40 +1,58 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.32.0"
-    }
+resource "azurerm_resource_group" "teckno" {
+  name     = "teckno-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "teckno" {
+  name                = "teckno-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.teckno.location
+  resource_group_name = azurerm_resource_group.teckno.name
+}
+
+resource "azurerm_subnet" "teckno" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.teckno.name
+  virtual_network_name = azurerm_virtual_network.teckno.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "teckno" {
+  name                = "teckno-nic"
+  location            = azurerm_resource_group.teckno.location
+  resource_group_name = azurerm_resource_group.teckno.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.teckno.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
-provider "azurerm" {
-  # Configuration options
-  subscription_id = var.subscription_id
-  features {
+resource "azurerm_linux_virtual_machine" "teckno" {
+  name                = "teckno-machine"
+  resource_group_name = azurerm_resource_group.teckno.name
+  location            = azurerm_resource_group.teckno.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.teckno.id,
+  ]
 
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
-}
 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-resource "azurerm_resource_group" "tf_rg" {
-  
-  name = var.resource_group_name
-  location = var.location
-}
-
-
-resource "azurerm_storage_account" "str1" {
-  name                     = var.storage1
-  resource_group_name      =  azurerm_resource_group.tf_rg.name
-  location                 = azurerm_resource_group.tf_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-
-
-resource "azurerm_storage_container" "example" {
-  name                  = var.container
-  storage_account_id    = azurerm_storage_account.str1.id
-  container_access_type = "private"
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
